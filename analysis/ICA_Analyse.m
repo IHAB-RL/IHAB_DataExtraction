58ee199062964b6859c5b5a162fe4def0d3a75bf
Joerg Bitzer <joerg.bitzer@jade-hs.de>
2019-09-08 23:35:23 +0200

    First analysis files

--- analysis/ICA_Analyse.m
+++ analysis/ICA_Analyse.m
@@ -2,15 +2,24 @@
 clear
 close all
 
+addpath('..');
+
 computeNew = 1;
 %szPersonDir = 'F:\IHAB_1_EMA2018\IHAB_Rohdaten_EMA2018\EK06DI26_180905_aw';
 %szPersonDir = 'F:\IHAB_2_EMA2018\IHAB_Rohdaten_EMA2018\ES04CH28_190423_aw';
+%szPersonDir =
+
+%'F:\IHAB_1_EMA2018\IHAB_Rohdaten_EMA2018\HM06LM23_181112_aw'; %
+%funktioniert nicht, wegen Problem mit chunkID Nr 11 in der Liste (vorletzte TE funktioniert auch nicht)
+
+
+
 szPersonDir = 'F:\HALLO_EMA2016\EMA_Rohdaten\CH04RT10_161122_fs';
 szBaseDir ='F:\IHAB_1_EMA2018\IHAB_Rohdaten_EMA2018';
 szOut = 'F:\Results';
 szDir = dir(szBaseDir);
 szDir(1:3) = [];
-for dd = 1:length(szDir)
+for dd = length(szDir):length(szDir)
     szPersonDir = [szBaseDir filesep szDir(dd).name]
     if (computeNew)
         obj = IHABdata(szPersonDir);
@@ -45,3 +54,4 @@ for dd = 1:length(szDir)
         save([szOut filesep obj.stSubject.Name '_' num2str(kk)],'obj', 'AllTime', 'AllData','kk','analDate');
     end
 end
+stop(timerfind)
