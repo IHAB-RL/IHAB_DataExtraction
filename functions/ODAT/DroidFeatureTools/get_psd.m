fa8c231dbb9a54d138abb8e802d942efe21df496
Pohlhausen <ju2646@hs-woe.de>
2019-09-23 12:31:09 +0200

    edit version history

--- functions/ODAT/DroidFeatureTools/get_psd.m
+++ functions/ODAT/DroidFeatureTools/get_psd.m
@@ -1,14 +1,23 @@
-function [Pxy, Pxx, Pyy] = get_psd(mData)
+function [Pxy, Pxx, Pyy] = get_psd(mData, version)
 
 % split PSDs 
 % mData: 514 Pxy (complex), 257 Pxx, 257: Pyy; x: left
 % mData: 1026 Pxy (complex), 513 Pxx, 513: Pyy; x: left
-% i.e: 
+
+
+if nargin < 2
+    version = 0;
+end
 
 n(1) = size(mData,2) / 2; 
 n(2) = n(1) / 2; 
 
-Pxy = ic2matc(mData(:,1:n(1)));
+if ~version % old
+    Pxy = ic2matc(mData(:,1:n(1)));
+else        % new
+    temp = mData(:,1:n(1));
+    Pxy = temp(:,1:2:end) + 1i*temp(:,2:2:end);
+end
 Pxx = mData(:,n(1)+1:n(1)+n(2));
 Pyy = mData(:,n(1)+n(2)+1:end);
 
