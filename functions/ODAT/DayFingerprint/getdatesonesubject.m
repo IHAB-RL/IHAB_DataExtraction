8b99e2dc33f19175bcb149cc70f6ad7b8c865783
Pohlhausen <ju2646@hs-woe.de>
2019-09-25 09:23:05 +0200

    updated to actual version

--- functions/ODAT/DayFingerprint/getdatesonesubject.m
+++ functions/ODAT/DayFingerprint/getdatesonesubject.m
@@ -1,22 +1,26 @@
-function T = getdatesonesubject(obj)
-%GETDATESONESUBJECT Gets all available dates from one subject
+function caDates = getdatesonesubject(obj)
+% function to get all dates of one subject
+%
+% Usage: caDates = getdatesonesubject(obj)
+%
+% Parameters
+% ----------
+% inParam :  obj - struct, contains all informations about the subject,
+%                  foldername, path...
+%
+% Returns
+% -------
+% outParam :  caDates - datetime array, contains all dates of one subject
+%
+% Author:  (c) TGM @ Jade Hochschule applied licence see EOF
+% Version History:
+% Ver. 0.01 initial create
+% Ver. 1.0 updated to actual version 25-Sep-2019 JP
 
-% Set os specific file separator once
-IFS = filesep;
-
-szDir = fullfile(pwd, 'IHAB_Rohdaten_EMA2018');
-
-% Get the names of all subject in the given directory
-% stSubject = getsubjectfolder(szDir,szSubject);
-% stSubject = struct('FolderName', obj.stSubject.Folder, 'SubjectID', obj.stSubject.Name);
-
-% if isempty(stSubject)
-%     error('Subject %s not found', szSubject);
-% end
 
 % List the whole content in the subject directory
-stFeatFiles = dir([obj.stSubject.Folder, filesep, ...
-    obj.stSubject.Name  '_AkuData']);
+stFeatFiles = dir([obj.szBaseDir filesep obj.stSubject.FolderName, filesep, ...
+    obj.stSubject.SubjectID  '_AkuData']);
 
 % Get rid of '.' and '..'
 stFeatFiles(1:2) = [];
@@ -31,9 +35,9 @@ caFileNames = {stFeatFiles.name};
 
 % Delete filenames belonging to corrupt files ...
 % (see ../Tools/CheckDataIntegrety.m)
-corruptTxtFile = fullfile(obj.stSubject.Folder,'corrupt_files.txt');
+corruptTxtFile = fullfile(obj.szBaseDir, obj.stSubject.FolderName,'corrupt_files.txt');
 if ~exist(corruptTxtFile,'file')
-    CheckDataIntegrety(stSubject.SubjectID);
+    CheckDataIntegrety(obj.stSubject.FolderName, obj.szBaseDir);
 end
 fid = fopen(corruptTxtFile,'r');
 corruptFiles = textscan(fid,'%s\n');
@@ -55,4 +59,27 @@ caUniqueDates = unique(caDatesWithoutTime);
 % Convert them to desired output format
 caDates = datetime(caUniqueDates,'InputFormat','yyyyMMdd');
 T = table();
-T.(obj.stSubject.Name) = caDates;
+T.(obj.stSubject.SubjectID) = caDates;
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <201x> 
+% Institute for Hearing Technology and Audiology
+% Jade University of Applied Sciences
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files
+% (the "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+% eof
--- functions/ODAT/DayFingerprint/getdatesonesubjectTest.m
+++ functions/ODAT/DayFingerprint/getdatesonesubjectTest.m
@@ -1,4 +1,39 @@
-%clearvars;
+% test script belonging to getdatesonesubject.m
+%
+% Author: (c) IHA @ Jade Hochschule applied licence see EOF
+% Version History:
+% Ver. 0.01 initial create 
+% Ver. 1.0 updated to actual version 25-Sep-2019 JP
 
-szSubject = 'ER08RD13';
-caDates = getdatesonesubject(szSubject);
+% path to data folder (needs to be customized)
+obj.szBaseDir = 'K:\IHAB_1_EMA2018\IHAB_Rohdaten_EMA2018';
+
+% name of subject folder
+szCurrentFolder =  'HN06MA10_180613_aw'; 
+obj.stSubject = getsubject(obj.szBaseDir, szCurrentFolder);
+
+% get all dates of one subject
+caDates = getdatesonesubject(obj);
+
+%--------------------Licence ---------------------------------------------
+% Copyright (c) <201x> 
+% Institute for Hearing Technology and Audiology
+% Jade University of Applied Sciences
+% Permission is hereby granted, free of charge, to any person obtaining
+% a copy of this software and associated documentation files
+% (the "Software"), to deal in the Software without restriction, including
+% without limitation the rights to use, copy, modify, merge, publish,
+% distribute, sublicense, and/or sell copies of the Software, and to
+% permit persons to whom the Software is furnished to do so, subject
+% to the following conditions:
+% The above copyright notice and this permission notice shall be included
+% in all copies or substantial portions of the Software.
+% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+% EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+% OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+% IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+% CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+% TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+% SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+% eof
