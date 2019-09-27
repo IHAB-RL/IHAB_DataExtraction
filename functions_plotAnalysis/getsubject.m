4a94920d8d3f3e39ae447f7adea09d69ba97097c
Pohlhausen <ju2646@hs-woe.de>
2019-09-27 15:47:04 +0200

    new input convention and some comments

--- functions_plotAnalysis/getsubject.m
+++ functions_plotAnalysis/getsubject.m
@@ -1,4 +1,28 @@
 function stSubject = getsubject(szBaseDir, szSubjectFolder)
+% function to get the name and path to the data folder of one subject
+% Usage stSubject = getsubject(szBaseDir, szSubjectFolder)
+%
+% Parameters
+% ----------
+% szBaseDir : string, contains path to base directory with all subjects
+%
+% szSubjectFolder : string, contains name of desired subject folder
+%                   if szBaseDir contains already the name of the subject 
+%                   folder, than szSubjectFolder must not be assigned
+% Returns
+% -------
+% stSubject : struct array with fields FolderName and SubjectID
+%
+% Author: J.Bitzer (c) TGM @ Jade Hochschule applied licence see EOF
+% Version History:
+% Ver. 0.01 initial create (empty) 
+% Ver. 1.0 new input convention and some comments 27-Sept-2019 JP
+
+if nargin == 1
+    caDir = regexpi(szBaseDir,'\','split');
+    szBaseDir = fullfile(caDir{1:end-1});
+    szSubjectFolder = caDir{end};
+end
 
 stSubject.FolderName = szSubjectFolder;
 caSubjectID = regexpi(szSubjectFolder,'[_]','split');
@@ -6,7 +30,6 @@ stSubject.SubjectID = caSubjectID{1};
 
 %% Check for existence
 % Get all sub-directories from base directory
-% subjectDirectories = dir(fullfile(pwd, 'IHAB_Rohdaten_EMA2018'));
 subjectDirectories = dir(szBaseDir);
 
 allSubjects = subjectDirectories([subjectDirectories.isdir]);
@@ -24,6 +47,5 @@ for ii = 1:length(allSubjects)
     if ii == length(allSubjects)
        error('Subject not found');
     end
-    
-    
+
 end
