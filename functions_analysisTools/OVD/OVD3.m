fbaa3d6df1c28250553897de1b7a420d0009ce0b
Pohlhausen <ju2646@hs-woe.de>
2019-11-13 17:19:44 +0100

    addd RMS

--- functions_analysisTools/OVD/OVD3.m
+++ functions_analysisTools/OVD/OVD3.m
@@ -1,8 +1,9 @@
-function [stDataReal] = OVD3(Cxy, Pxx, Pyy, fs)
+function [stDataReal] = OVD3(Cxy, Pxx, Pyy, fs, mRMS)
 % Cxy - cross power spectral density
 % Pxx, Pyy - auto power spectral density
 % data format: time x frequency
 % fs - sampling frequency
+% optional mRMS - rms vector in dB SPL
 
 % number of frames
 nFrames = size(Cxy,1);
@@ -28,7 +29,7 @@ curPyy = zeros(nFrames, nFreqBins);
 % MIN_RMS = 10^(-40/20);
 %% JP
 MIN_COH = 0.3;
-MIN_RMS = 10^(-45/20);
+MIN_RMS = 10^(-45/20); % -> for dB FS
 
 % smoothing PSD data for 3 adjacent frames respectively
 for iFrame = 1:nFrames
@@ -51,10 +52,16 @@ end
 % on noise and not on levels with speech
 [A_log,A_lin] = a_weighting(nFFT,fs);
 
-% Divide by 4 as a scaling to RMS feature; has to be checked if real
-% features are used
-stDataReal.curRMSfromPxx = sqrt(sum((fs/nFFT).*curPxx, 2))./4;
-stDataReal.curRMS_A = sqrt(sum((fs/nFFT).*curPxx'.*A_lin/10^(-2/20)))./4;
+if ~exist('mRMS', 'var')
+    % Divide by 4 as a scaling to RMS feature; has to be checked if real
+    % features are used
+    stDataReal.curRMSfromPxx = sqrt(sum((fs/nFFT).*curPxx, 2))./4;
+    stDataReal.curRMS_A = sqrt(sum((fs/nFFT).*curPxx'.*A_lin/10^(-2/20)))./4;
+else
+    stDataReal.curRMSfromPxx = mRMS(:,1);
+    
+    MIN_RMS = 30; % dB SPL
+end
 
 % Calculate coherence
 stDataReal.Coh = curCxy./(sqrt(curPxx.*curPyy) + eps);
@@ -86,9 +93,10 @@ end
 stDataReal.movAvgSNR = movmax(stDataReal.meanLogSNRPrio,stDataReal.winLen);
 
 % This parameters are set dependent on 'overall level'
+% stDataReal.a_rms = 0.15.*ones(nFrames, 1); % NS
+stDataReal.a_rms = 0.5.*ones(nFrames, 1); % JP
+% stDataReal.a_rms(stDataReal.movAvgSNR >= 30) = 0.2;
 stDataReal.a_cohe = 0.3.*ones(nFrames, 1);
-stDataReal.a_rms = 0.15.*ones(nFrames, 1);
-stDataReal.a_rms(stDataReal.movAvgSNR >= 30) = 0.2;
 stDataReal.a_cohe(stDataReal.movAvgSNR >= 20) = 0.5;
 stDataReal.a_cohe(stDataReal.movAvgSNR >= 30) = 0.4;
 stDataReal.a_cohe(stDataReal.movAvgSNR >= 40) = 0.5;
@@ -97,8 +105,8 @@ stDataReal.a_cohe(stDataReal.movAvgSNR >= 50) = 0.4;
 % Sliding max and min
 stDataReal.adapThreshMax = movmax(stDataReal.meanCoheTimesCxy,stDataReal.winLen);
 stDataReal.adapThreshMin = movmin(stDataReal.meanCoheTimesCxy,stDataReal.winLen);
-stDataReal.adapThreshMaxRMS = movmax(stDataReal.curRMSfromPxx,stDataReal.winLen.*1.5);
-stDataReal.adapThreshMinRMS = movmin(stDataReal.curRMSfromPxx,stDataReal.winLen.*1.5);
+stDataReal.adapThreshMaxRMS = movmax(stDataReal.curRMSfromPxx,stDataReal.winLen);
+stDataReal.adapThreshMinRMS = movmin(stDataReal.curRMSfromPxx,stDataReal.winLen);
 
 % Adaptive thresholds
 stDataReal.adapThreshCohe = (stDataReal.a_cohe.*stDataReal.adapThreshMax + (1-stDataReal.a_cohe).*stDataReal.adapThreshMin);
