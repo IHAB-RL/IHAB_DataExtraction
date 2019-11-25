f4d9dab1edd5816a096366e3b829035ecd4dca26
Pohlhausen <ju2646@hs-woe.de>
2019-11-25 17:10:44 +0100

    train with MFCCs

--- functions_application/evaluation/RandomForest/RandomForest.m
+++ functions_application/evaluation/RandomForest/RandomForest.m
@@ -12,23 +12,24 @@ clear;
 % vNames = {'Re(Cohe)'; 'RMS1'; 'RMS2'; 'ZCR1'; 'ZCR2'; 'ZCRdiff1'; 'ZCRdiff2'; 'EQD1'; 'EQD2'; 'MeanSPP'; 'rms(Corr)'};
 % szVarNames = {'mMeanRealCoherence', 'mRMS', 'Cxy', 'mEQD', 'mMeanSPP', 'mCorrRMS', 'vGroundTruthVS'};
 % vNames = {'Re(Cohe)'; 'RMS1'; 'RMS2'; 'CPSD1'; 'CPSD2'; 'CPSD3'; 'CPSD4'; 'CPSD5'; 'CPSD6'; 'CPSD7'; 'CPSD8'; 'CPSD9'; 'CPSD10'; 'CPSD11'; 'CPSD12'; 'EQD1'; 'EQD2'; 'MeanSPP'; 'rms(Corr)'};
-szVarNames = {'mMeanRealCoherence', 'Pxx', 'Cxy', 'vGroundTruthVS'};
-vNames = {'Re(Cohe)'; 'APSD1'; 'APSD2'; 'APSD3'; 'APSD4'; 'APSD5'; 'APSD6'; 'APSD7'; 'APSD8'; 'APSD9'; 'APSD10'; 'APSD11'; 'APSD12'; 'CPSD1'; 'CPSD2'; 'CPSD3'; 'CPSD4'; 'CPSD5'; 'CPSD6'; 'CPSD7'; 'CPSD8'; 'CPSD9'; 'CPSD10'; 'CPSD11'; 'CPSD12'};
+% szVarNames = {'mMeanRealCoherence', 'Pxx', 'Cxy', 'vGroundTruthVS'};
+% vNames = {'Re(Cohe)'; 'APSD1'; 'APSD2'; 'APSD3'; 'APSD4'; 'APSD5'; 'APSD6'; 'APSD7'; 'APSD8'; 'APSD9'; 'APSD10'; 'APSD11'; 'APSD12'; 'CPSD1'; 'CPSD2'; 'CPSD3'; 'CPSD4'; 'CPSD5'; 'CPSD6'; 'CPSD7'; 'CPSD8'; 'CPSD9'; 'CPSD10'; 'CPSD11'; 'CPSD12'};
+szVarNames = {'mMeanRealCoherence', 'mRMS', 'mZCR', 'mfcc', 'mEQD', 'mMeanSPP', 'mCorrRMS', 'vGroundTruthVS'};
+vNames = {'Re(Cohe)'; 'RMS1'; 'RMS2'; 'ZCR1'; 'ZCR2'; 'ZCRdiff1'; 'ZCRdiff2'; 'MFCC1'; 'MFCC2'; 'MFCC3'; 'MFCC4'; 'MFCC5'; 'MFCC6'; 'MFCC7'; 'MFCC8'; 'MFCC9'; 'MFCC10'; 'MFCC11'; 'MFCC12'; 'MFCC13'; 'EQD1'; 'EQD2'; 'MeanSPP'; 'rms(Corr)'};
 
 
 % create training data set
 isTraining = true;
-[mDataSet, vVoiceLabels] = createTestSet(szVarNames, isTraining);
+isOnlyOVD = true;
+[mDataSet, vVoiceLabels] = createTestSet(szVarNames, isTraining, isOnlyOVD);
 
 % set number of trees
 nTrees = 50;
 
 %% Train an ensemble of bagged classification trees 
-tic
 Mdl = TreeBagger(nTrees, mDataSet, vVoiceLabels, 'OOBPrediction', 'On',...
     'Method', 'classification', 'OOBPredictorImportance', 'On',...
     'PredictorNames', vNames); % 'Prior', 'Uniform'
-toc
 
 % % view for example decision tree nr. 1
 % view(Mdl.Trees{1},'Mode','graph');
@@ -57,14 +58,18 @@ ylabel 'Out-of-bag classification error';
 %% Test the ensemble of bagged classification trees 
 % create test data set
 isTraining = false;
-[mTestDataSet, vGroundTruthVS] = createTestSet(szVarNames, isTraining);
+[mTestDataSet, vGroundTruthVS] = createTestSet(szVarNames, isTraining, isOnlyOVD);
 
 % start prediction with trained ensemble of bagged classification trees
 vPredictedVS = predict(Mdl, mTestDataSet);
 vPredictedVS = str2num(cell2mat(vPredictedVS));
 
 % display results as confusion matrix
-vLabels = {'no VS', 'OVS', 'FVS'};
+if isOnlyOVD
+    vLabels = {'no VS', 'OVS'};
+else
+    vLabels = {'no VS', 'OVS', 'FVS'};
+end
 plotConfusionMatrix([], vLabels, vGroundTruthVS, vPredictedVS)
 
 
